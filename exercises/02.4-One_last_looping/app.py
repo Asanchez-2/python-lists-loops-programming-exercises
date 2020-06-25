118bc9fd68d3bcfcbd6145b65a3028b806dd5a61
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-06-25 16:02:41 +0000

    Done hasta 4 included

--- exercises/02.4-One_last_looping/app.py
+++ exercises/02.4-One_last_looping/app.py
@@ -1,4 +1,10 @@
 my_sample_list = ['Esmeralda','Kiko','Ruth','Lebron','Pedro','Maria','Lou','Fernando','Cesco','Bart','Annie']
 
 #Your code here:
-
+my_sample_list[1]= 'Steven'
+my_sample_list_lenght = len(my_sample_list)-1
+my_sample_list[my_sample_list_lenght] = 'Pepe'
+my_sample_list[0] = my_sample_list[2]+my_sample_list[4]
+# print(my_sample_list[1])
+for i in range(my_sample_list_lenght,-1,-1):
+    print(my_sample_list[i])
--- exercises/03-flip_list/app.py
+++ exercises/03-flip_list/app.py
@@ -1,3 +1,9 @@
 arr = [45, 67, 87, 23, 5,  32, 60]
-
 #your code below:
+# arr.reverse()
+# print(arr)
+arr_lenght = len(arr)-1
+new_list_variable = []
+for i in range(6,-1,-1):
+    new_list_variable.append(arr[i])
+print(new_list_variable)
