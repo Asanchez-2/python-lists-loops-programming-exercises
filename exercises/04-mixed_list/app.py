9718d75424a61d955002b4be018148cf83f4c86b
Ángel Sánchez <hangelsanchez2@hotmail.com>
2020-06-25 16:27:40 +0000

    En proceso ejercicio 4.1

--- exercises/04-mixed_list/app.py
+++ exercises/04-mixed_list/app.py
@@ -1,3 +1,7 @@
 mix = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 # Your code below:
+# print(type(mix[0]))
+mix_lenght = len(mix)
+for i in range(0,mix_lenght):
+    print(type(mix[i]))
--- exercises/04.1-count_on/app.py
+++ exercises/04.1-count_on/app.py
@@ -1,7 +1,12 @@
-
+hello = []
 my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
+my_list_lenght = len(my_list)
+print(my_list_lenght)
 
 
 #your code go here:
+for i in range(0,my_list_lenght):
+     if type(my_list[i]) == 'dict':
+         hello = my_list.append[i]
 
 print(hello)
